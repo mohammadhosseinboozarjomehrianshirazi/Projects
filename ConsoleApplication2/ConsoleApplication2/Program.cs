f20847a60a648011a7d7e5195d94464837c2f1a8
mohammadhosseinboozarjomehrianshirazi <arshia.shirazi1211@gmail.com>
2023-05-26 02:31:53 +0430

    name

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("enter your name");
+            name = Console.ReadLine();
+            Console.WriteLine("hello" + name);
+            Console.WriteLine("press any key exit...");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine("plz enter num:");
+            string number = Console.ReadLine();
+            x = int.Parse(number);
+            x *= 3;
+            Console.WriteLine(x);
+            Console.WriteLine("press any key exit...");
+            Console.ReadKey();
+        }
+    }
+}
